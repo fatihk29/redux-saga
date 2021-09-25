716fc004f464dd6e984060a4454bdab16ea90991
fatihk29 <fatihkrc7@gmail.com>
2021-09-25 14:59:04 +0300

    second arrangments

--- src/App.js
+++ src/App.js
@@ -1,77 +1,27 @@
-import { Container, Grid, Icon, Segment } from "semantic-ui-react";
+import { Container, Segment } from "semantic-ui-react";
 
 import "./App.css";
 import ButtonSaveOrCancel from "./components/ButtonSaveOrCancel";
 import DisplayBalance from "./components/DisplayBalance";
+import DisplayBalances from "./components/DisplayBalances";
 import MainHeader from "./components/MainHeader";
 import NewEntryForm from "./components/NewEntryForm";
+import EntryLine from "./components/EntryLine";
 
 function App() {
   return (
     <Container>
       <MainHeader title="Budget" />
       <DisplayBalance title="Your Balance" value="2,550" />
-      <Segment textAlign="center">
-        <Grid columns={2} divided>
-          <Grid.Row>
-            <Grid.Column>
-              <DisplayBalance title="Income" value="1253.54" color="green" />
-            </Grid.Column>
-            <Grid.Column>
-              <DisplayBalance title="Enpenses" value="632,5" color="red" />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <DisplayBalances />
       <MainHeader type="h3" title="History" />
-      <Segment color="red">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign="left">
-              Something
-            </Grid.Column>
-            <Grid.Column width={3} textAlign="left">
-              $100
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash" bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment color="green">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign="left">
-              Something
-            </Grid.Column>
-            <Grid.Column width={3} textAlign="left">
-              $100
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash" bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment color="red">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign="left">
-              Something
-            </Grid.Column>
-            <Grid.Column width={3} textAlign="left">
-              $100
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash" bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+
+      <EntryLine description="income" value="10" isExpense />
+
+      <EntryLine />
+
+      <EntryLine />
+
       <MainHeader type="h3" title="Add New Transaction" />
       <NewEntryForm />
       <ButtonSaveOrCancel />
