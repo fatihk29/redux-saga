748edaf484c04b8c32fb63e4c129fbb79ad6b428
fatihk29 <fatihkrc7@gmail.com>
2021-10-12 23:33:27 +0300

    updated

--- src/saga/entriesSaga.js
+++ src/saga/entriesSaga.js
@@ -1,4 +1,4 @@
-import { call, take, put } from "redux-saga/effects";
+import { call, fork, take, put } from "redux-saga/effects";
 import entriesTypes from "../actions/entries.actions";
 import axios from "axios";
 import types from "../actions/entries.actions";
@@ -9,4 +9,19 @@ export function* getAllEntries() {
   const result = yield call(axios, "http://localhost:3001/entries");
   console.log("result", result);
   yield put({ type: types.POPULATE_ENTRIES, payload: result.data });
+}
+
+export function* getEntryDetails(id) {
+  console.log(`got the id ${id}`);
+  const { data } = yield call(axios, `http://localhost:3001/values/${id}`);
+  console.log("data", data);
+}
+
+export function* getAllEntriesDetails() {
+  const { payload } = yield take(entriesTypes.POPULATE_ENTRIES);
+  console.log("length", payload);
+  for (let index = 0; index < payload.length - 1; ++index) {
+    const entry = payload[index];
+    yield fork(getEntryDetails, entry.id);
+  }
 }
